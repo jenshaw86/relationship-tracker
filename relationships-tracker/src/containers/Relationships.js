e8e7ccc51a87cfae958bdf4e8ab1396e4c5ff4f9
jenshaw86 <bloo.j.shaw@gmail.com>
2019-08-09 09:33:45 -0700

    Add react-bootstrap

--- relationships-tracker/src/containers/Relationships.js
+++ relationships-tracker/src/containers/Relationships.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import RelationshipCard from '../components/RelationshipCard'
+import Button from 'react-bootstrap/Button';
 
 // props: location, history, match, relationships(collection), getRelationshipProfile(f), 
 
@@ -16,7 +17,7 @@ const Relationships = (props) => {
      return (
          <div>
             <h1>All Relationships</h1>
-            
+            <Button variant="primary">Primary</Button>
             { displayAllRelationships() }
          </div> 
      )
